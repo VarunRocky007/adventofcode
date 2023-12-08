b5eb0cbc7d314ad0645a2830bf412b9eb99fd889
VarunRocky007 <varsho2002@gmail.com>
2023-12-08 10:51:59 +0530

    day two problem 1st question solution [Varun]

--- day_two_problem.py
+++ day_two_problem.py
@@ -1,6 +1,32 @@
 import fileinput
 
-list_of_games = []
+list_of_games = {}
+count = 1
 for games in fileinput.input(files="day_two_input"):
-    list_of_games.append(games.split(":")[-1].strip())
-print(list_of_games)
+    list_of_games.update({count: games.split(":")[-1].strip().split(";")})
+    count += 1
+processed_list_of_games = {}
+possible_games = 0
+for game in list_of_games.keys():
+    prob_of_cube = []
+    for probs in list_of_games[game]:
+        prob_of_colors = {"red": 0, "green": 0, "blue": 0}
+        for prob in probs.split(","):
+            prob_of_colors.update({prob.strip().split()[-1]:int(prob.strip().split()[0])})
+        prob_of_cube.append(prob_of_colors)
+    processed_list_of_games.update({game:prob_of_cube})
+print(processed_list_of_games)
+for game in processed_list_of_games.keys():
+    temp = False
+    for probs_of_cube in processed_list_of_games[game]:
+        if probs_of_cube["red"] <= 12 and probs_of_cube["blue"] <= 14 and probs_of_cube["green"] <= 13:
+            temp = True
+        else:
+            temp = False
+            break
+    if temp:
+        possible_games+=game
+print(possible_games)
+
+
+
